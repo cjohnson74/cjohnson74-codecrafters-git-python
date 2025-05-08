5a6e6601b14d49beb250f9dabcf8fe1c1e36c3f2
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-08 16:13:19 -0400

    git ls-tree

--- app/main.py
+++ app/main.py
@@ -3,6 +3,27 @@ import os
 import zlib
 import hashlib
 
+def cat_file(blob):
+    folder = blob[:2]
+    file = blob[2:]
+    with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
+        contents = zlib.decompress(blob_file.read()).decode("utf-8")
+        type = contents.split(" ")[0]
+        content = contents.split("\0")[1]
+        return (type, content)
+    
+def hash_object(file):
+    with open(file) as file:
+        data = file.read()
+        blob_object = f"blob {len(data)}\0{data}"
+        sha = hashlib.sha1(blob_object.encode("utf-8")).hexdigest()
+        file_path = f".git/objects/{sha[:2]}/{sha[2:]}"
+        os.makedirs(os.path.dirname(file_path))
+        with open(file_path, "wb") as file:
+            file.write(zlib.compress(blob_object.encode("utf-8")))
+        return sha
+    
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -20,24 +41,16 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         blob = sys.argv[sys.argv.index("-p") + 1]
-        folder = blob[:2]
-        file = blob[2:]
-        with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
-            contents = zlib.decompress(blob_file.read()).decode("utf-8")
-            type = contents.split(" ")[0]
-            content = contents.split("\0")[1]
-            print(content, end="")
+        (type, content) = cat_file(blob)
+        print(content, end="")
     elif command == "hash-object":
         file = sys.argv[sys.argv.index("-w") + 1]
-        with open(file) as file:
-            data = file.read()
-            blob_object = f"blob {len(data)}\0{data}"
-            sha = hashlib.sha1(blob_object.encode("utf-8")).hexdigest()
-            file_path = f".git/objects/{sha[:2]}/{sha[2:]}"
-            os.makedirs(os.path.dirname(file_path))
-            with open(file_path, "wb") as file:
-                file.write(zlib.compress(blob_object.encode("utf-8")))
-            print(sha)
+        sha = hash_object(file)
+        print(sha)
+    elif command == "ls-tree":
+        tree_sha = sys.argv[sys.argv.index("ls-tree") + 1]
+        (type, content) = hash_object(tree_sha)
+        print(f"type: {type}, contents: {content}")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
