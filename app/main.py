503237116c7ea3330c4c86347221559a2f1280d1
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-07 20:55:51 -0400

    git read object

--- app/main.py
+++ app/main.py
@@ -19,10 +19,8 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         blob = sys.argv[sys.argv.index("-p") + 1]
-        print(f"blob: {blob}")
         folder = blob[:2]
         file = blob[2:]
-        print(f"folder: {folder}, file: {file}")
         with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
             contents = zlib.decompress(blob_file.read()).decode("utf-8")
             type = contents.split(" ")[0]
