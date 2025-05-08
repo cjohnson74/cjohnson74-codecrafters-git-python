5da1eef839ef9bbf7736c016100f4c004bcdf111
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-08 19:26:08 -0400

    git ls-tree

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import os
 import zlib
 import hashlib
 
-def cat_file(blob):
-    folder = blob[:2]
-    file = blob[2:]
+def read_blob_object(sha):
+    folder = sha[:2]
+    file = sha[2:]
     with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
         contents = blob_file.read()
         contents = zlib.decompress(contents).decode("utf-8")
@@ -24,30 +24,37 @@ def hash_object(file):
             file.write(zlib.compress(blob_object.encode("utf-8")))
         return sha
     
-def read_tree_object(blob):
-    (type, content) = cat_file(blob)
-    index = 0
-    entries = []
-    
-    while index < len(content):
-        space_pos = content.find(b' ', index)
-        file_mode = content[index:space_pos].decode("utf-8")
-        index = space_pos + 1
+def read_tree_object(sha):
+    folder = sha[:2]
+    file = sha[2:]
+    with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
+        data = blob_file.read()
+        data = zlib.decompress(data)
+        null_pos = data.index(b' ')
+        content = data[null_pos + 1:]
         
-        null_pos = content.find(b'\0', index)
-        file_name = content[index:null_pos].decode("utf-8")
-        index = null_pos + 1
+        index = 0
+        entries = []
         
-        raw_sha = content[index:index + 20]
-        sha_hex = raw_sha.hex()
-        index += 20
-        
-        entries.append({
-            "mode": file_mode,
-            "name": file_name,
-            "sha": sha_hex
-        })
-    return entries
+        while index < len(content):
+            space_pos = content.find(b' ', index)
+            file_mode = content[index:space_pos].decode("utf-8")
+            index = space_pos + 1
+            
+            null_pos = content.find(b'\0', index)
+            file_name = content[index:null_pos].decode("utf-8")
+            index = null_pos + 1
+            
+            raw_sha = content[index:index + 20]
+            sha_hex = raw_sha.hex()
+            index += 20
+            
+            entries.append({
+                "mode": file_mode,
+                "name": file_name,
+                "sha": sha_hex
+            })
+        return entries
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -65,7 +72,7 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         blob = sys.argv[sys.argv.index("-p") + 1]
-        (type, content) = cat_file(blob)
+        (type, content) = read_blob_object(blob)
         print(content, end="")
     elif command == "hash-object":
         file = sys.argv[sys.argv.index("-w") + 1]
