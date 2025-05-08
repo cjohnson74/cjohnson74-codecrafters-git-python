eb6d94794dbc2b1b2bd7e9034102f52df3215ede
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-08 17:50:14 -0400

    git ls-tree

--- app/main.py
+++ app/main.py
@@ -3,7 +3,33 @@ import os
 import zlib
 import hashlib
 
-def read_obj(blob):
+def cat_file(blob):
+    folder = blob[:2]
+    file = blob[2:]
+    print(f"folder: {folder}, file: {file}")
+    with open(f".git/objects/{folder}/{file}", "rb") as blob_file:
+        contents = blob_file.read()
+        print(contents)
+        contents = zlib.decompress(contents)
+        print(contents)
+        # contents = contents.decode("utf-8")
+        # print(contents)
+        type = contents.split(" ")[0]
+        content = contents.split("\0")[1]
+        return (type, content)
+    
+def hash_object(file):
+    with open(file) as file:
+        data = file.read()
+        blob_object = f"blob {len(data)}\0{data}"
+        sha = hashlib.sha1(blob_object.encode("utf-8")).hexdigest()
+        file_path = f".git/objects/{sha[:2]}/{sha[2:]}"
+        os.makedirs(os.path.dirname(file_path))
+        with open(file_path, "wb") as file:
+            file.write(zlib.compress(blob_object.encode("utf-8")))
+        return sha
+    
+def read_tree_object(blob):
     folder = blob[:2]
     file = blob[2:]
     print(f"folder: {folder}, file: {file}")
@@ -38,27 +64,6 @@ def read_obj(blob):
                 "sha": sha_hex
             })
         return entries
-            
-        contents = zlib.decompress(contents)
-        print(contents)
-        # contents = contents.decode("utf-8")
-        # print(contents)
-        type = contents.split(" ")[0]
-        content = contents.split("\0")[1]
-        return (type, content)
-    
-def write_obj(file):
-    with open(file) as file:
-        data = file.read()
-        blob_object = f"blob {len(data)}\0{data}"
-        sha = hashlib.sha1(blob_object.encode("utf-8")).hexdigest()
-        file_path = f".git/objects/{sha[:2]}/{sha[2:]}"
-        os.makedirs(os.path.dirname(file_path))
-        with open(file_path, "wb") as file:
-            file.write(zlib.compress(blob_object.encode("utf-8")))
-        return sha
-    
-
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -76,15 +81,15 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         blob = sys.argv[sys.argv.index("-p") + 1]
-        (type, content) = read_obj(blob)
+        (type, content) = cat_file(blob)
         print(content, end="")
     elif command == "hash-object":
         file = sys.argv[sys.argv.index("-w") + 1]
-        sha = write_obj(file)
+        sha = hash_object(file)
         print(sha)
     elif command == "ls-tree":
         tree_sha = sys.argv[sys.argv.index("--name-only") + 1]
-        (type, content) = read_obj(tree_sha)
+        entries = read_tree_object(tree_sha)
         print(f"type: {type}, contents: {content}")
     else:
         raise RuntimeError(f"Unknown command #{command}")
