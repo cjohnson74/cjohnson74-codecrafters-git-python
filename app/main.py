c553d85a0dd2d000c55a178abef99a60bfbeaf82
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-09 07:21:09 -0400

    git write-tree

--- app/main.py
+++ app/main.py
@@ -80,7 +80,7 @@ def write_tree(directory):
         entries.append(f"{mode} {entry}\0".encode() + bytes.fromhex(sha))
         
     tree_content = b"".join(entries)
-    print(repr(tree_content))
+    # print(repr(tree_content))
     return hash_object(tree_content, obj_type="tree")
         
 
