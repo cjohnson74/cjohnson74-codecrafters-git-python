decb5508854b995389a2d37da6259bbf80b6a048
Carson Johnson <cjohnson74.tech@gmail.com>
2025-05-16 21:10:06 -0400

    git checkout tree

--- app/network.py
+++ app/network.py
@@ -124,7 +124,7 @@ def fetch_pack_file(git_url):
                         break
                     packfile_response.extend(data)  
                 
-                print(f"Packfile Response: {packfile_response[5000:6000]}") 
+                print(f"Packfile Response: {packfile_response[6000:7000]}") 
                                
     except (socket.error, ssl.SSLError) as e:
         raise RuntimeError(f"Failed to send request to {host}:{port} - {e}") from e
